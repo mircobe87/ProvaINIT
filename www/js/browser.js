85fac2b7d0c82a54718cd87021da2d4b5811f943
Mirco Bertelli <mircobe87@gmail.com>
2013-10-17 12:28:11 +0200

    Aggiunta Sezione Browser
    Creato il contenuto della view che mostra le informazioni relative al
    browser prese dall'oggetto window.navigator del DOM e creato anche il
    file JavaScrip js/browser.js che riempie la view con i relativi valori.

---
+++ www/js/browser.js
@@ -0,0 +1,16 @@
+(function(){
+	//Browser
+	$("#b-codeName").text(navigator.appCodeName);
+	$("#b-name").text(navigator.appName);
+	$("#b-version").text(navigator.appVersion);
+
+	// Cookie
+	$("#b-cookie").text((navigator.cookieEnabled)?"Abilitati":"Disabilitati");
+
+	// Piattaforma
+	$("#b-platform").text(navigator.platform);
+
+	// User-Agent
+	$("#b-useragent-h").text(navigator.userAgent);
+	$("#b-useragent-l").text(navigator.systemLanguage);
+})();
